43aef90cc8ce00f353f0a5d7a57a3ab96cb85d11
haliphax <haliphax@gmail.com>
2021-09-29 18:42:26 -0500

    ✨ simple twitch chat command to start drop

--- drop-game/static/index.js
+++ drop-game/static/index.js
@@ -1,24 +1,25 @@
 const GRAVITY = 60;
 
+const qs = Object.fromEntries(
+	window.location.href.split('?')[1].split('&').map(v => v.split('=')));
+const twitch = new tmi.Client({
+	channels: [qs.channel],
+	identity: {
+		username: qs.username,
+		password: `oauth:${qs.oauth}`,
+	},
+});
+
+const emitter = new Phaser.Events.EventEmitter();
+
 class Game extends Phaser.Scene {
 	constructor() {
 		super();
 	}
 
 	create() {
-		const direction = Math.random() <= 0.5 ? -1 : 1;
-		const velocity = Math.random() * 1000 * direction;
-
+		emitter.on('drop', this.onDrop, this);
 		this.physics.world.setBounds(0, 0, 1920, 1080);
-
-		this.dop = this.physics.add.image(0, 0, 'dop');
-		this.dop
-			.setOrigin(0, 0)
-			.setBounce(1)
-			.setCollideWorldBounds(true)
-			.setMaxVelocity(1000, GRAVITY)
-			.setVelocity(velocity, 0);
-
 		this.pad = this.physics.add.image(0, 120, 'pad');
 		this.pad.body.allowGravity = false;
 		this.pad.body.immovable = true;
@@ -27,47 +28,40 @@ class Game extends Phaser.Scene {
 		this.pad
 			.setOrigin(0, 0)
 			.setScale(2);
-
-		this.collider = this.physics.add.collider(
-			this.dop,
-			this.pad,
-			(dop, pad) => {
-				if (dop.body.touching.down && pad.body.touching.up) {
-					this.add.image(this.dop.x, this.dop.y, 'dop')
-						.setOrigin(0, 0)
-						.setAlpha(0.5);
-
-					const halfPad = Math.ceil(this.pad.width / 2);
-					const halfDop = Math.ceil(this.dop.width / 2);
-					const total = halfPad + halfDop;
-					const pos = Math.abs(
-						(this.dop.x + halfDop) - (this.pad.x + halfPad));
-					const score = (total - pos) / total * 100;
-
-					console.log(score);
-					this.dop.destroy();
-					delete this.dop;
-				}
-			});
 	}
 
 	preload() {
 		this.load.setBaseURL('/assets');
-		this.load.image('dop', 'dop.png');
-		this.load.image('pad', 'trampoline.png');
+		this.load.image('drop', 'drop.png');
+		this.load.image('pad', 'pad.png');
 	}
 
 	update(time, delta) {
-		if (this.dop == undefined) return;
+		if (this.drop == undefined) return;
 
-		//this.pad.x = this.dop.getCenter().x + (this.dop.width / 2);
-
-		if (this.dop.y > 0
-			&& Phaser.Math.Fuzzy.LessThan(this.dop.body.velocity.y, 0, 0.1))
+		if (this.drop.y > 0
+			&& Phaser.Math.Fuzzy.LessThan(this.drop.body.velocity.y, 0, 0.1))
 		{
-			this.dop.destroy();
-			delete this.dop;
+			this.drop.destroy();
+			delete this.drop;
 		}
+	}
+
+	// events
+
+	onDrop() {
+		if (this.drop != undefined) return;
+
+		const direction = Math.random() <= 0.5 ? -1 : 1;
+		const velocity = Math.random() * 1000 * direction;
+
+		this.drop = this.physics.add.image(0, 0, 'drop');
+		this.drop
+			.setOrigin(0, 0)
+			.setBounce(1)
+			.setCollideWorldBounds(true)
+			.setMaxVelocity(1000, GRAVITY)
+			.setVelocity(velocity, 0);
 	}
 }
 
@@ -90,3 +84,12 @@ const game = new Phaser.Game({
 	type: Phaser.AUTO,
 	width: 1920,
 });
+
+const dropCommandRgx = /^\!drop(?:\s*([^ ]+)?)/i;
+twitch.on('message', (channel, tags, message, self) => {
+	if (self || !dropCommandRgx.exec(message)) return;
+
+	emitter.emit('drop');
+});
+
+twitch.connect();
