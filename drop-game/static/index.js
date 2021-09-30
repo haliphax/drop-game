d0b709160eb32aebfcb02b4bf7f89d72c4fe3754
haliphax <haliphax@gmail.com>
2021-09-29 21:16:31 -0500

    🐛 actually fix score!

--- drop-game/static/index.js
+++ drop-game/static/index.js
@@ -22,14 +22,19 @@ class Game extends Phaser.Scene {
 	create() {
 		emitter.on('drop', this.onDrop, this);
 		this.physics.world.setBounds(0, 0, 1920, 1080);
-		this.pad = this.physics.add.image(0, 120, 'pad');
+		this.pad = this.physics.add.image(0, 0, 'pad');
 		this.pad.body.allowGravity = false;
 		this.pad.body.immovable = true;
+		this.pad.body.setSize(this.pad.width, this.pad.height, true);
 		this.pad.x = Math.random() * (1920 - this.pad.width * 2);
 		this.pad.y = 1080 - this.pad.height * 2;
 		this.pad
 			.setOrigin(0, 0)
 			.setScale(2);
+
+		setTimeout(function() {
+			this.pad.body.setSize(this.pad.width, this.pad.height, true);
+		}, 1000);
 	}
 
 	preload() {
@@ -45,6 +50,9 @@ class Game extends Phaser.Scene {
 		if (this.drop.y > 0
 			&& Phaser.Math.Fuzzy.LessThan(this.drop.body.velocity.y, 0, 0.1))
 		{
+			this.add.image(this.drop.x, this.drop.y, 'drop')
+				.setOrigin(0, 0)
+				.setAlpha(0.25);
 			this.drop.destroy();
 			delete this.drop;
 		}
@@ -63,22 +71,27 @@ class Game extends Phaser.Scene {
 			.setOrigin(0, 0)
 			.setBounce(1)
 			.setCollideWorldBounds(true)
-			.setMaxVelocity(1000, GRAVITY)
-			.setVelocity(velocity, 0);
+			.setMaxVelocity(1000, GRAVITY);
+		this.drop.x = Math.random() * (1920 - this.drop.width);
+		this.drop.body.velocity.x = velocity;
+
+		setTimeout(function() {
+			this.drop.body.setSize(this.drop.width, this.drop.height, true);
+		}, 1000);
+
 		this.collider = this.physics.add.collider(
 			this.drop,
 			this.pad,
 			(drop, pad) => {
 				if (drop.body.touching.down && pad.body.touching.up) {
 					this.add.image(this.drop.x, this.drop.y, 'drop')
-						.setOrigin(0, 0)
-						.setAlpha(0.5);
+						.setOrigin(0, 0);
 
-					const halfPad = Math.ceil(this.pad.width / 2);
-					const halfDop = Math.ceil(this.drop.width / 2);
-					const total = halfPad + halfDop;
+					const halfPad = Math.ceil(this.pad.body.width / 2);
+					const halfDrop = Math.ceil(this.drop.body.width / 2);
+					const total = halfPad + halfDrop;
 					const pos = Math.abs(
-						(this.drop.x + halfDop) - (this.pad.x + halfPad));
+						this.drop.getCenter().x - this.pad.getCenter().x);
 					const score = ((total - pos) / total * 100).toFixed(2);
 
 					const label = this.add.text(0, 0, score,
@@ -86,13 +99,12 @@ class Game extends Phaser.Scene {
 							fontFamily: '"Syne Mono"',
 							fontSize: 26,
 							stroke: '#000',
-							strokeThickness: 1,
+							strokeThickness: 2,
 						});
 
 					label.x = this.drop.getCenter().x - label.width / 2;
 					label.y = this.drop.y + this.drop.height - label.height;
 
-					console.log(score);
 					this.drop.destroy();
 					delete this.drop;
 				}
