edaac2d8295bdba49f8303157214627ba23f53fe
haliphax <haliphax@gmail.com>
2021-10-04 23:54:38 -0500

    🎨 format webfontfile because it bothers me

--- drop-game/static/webfontfile.js
+++ drop-game/static/webfontfile.js
@@ -11,33 +11,31 @@ export default class WebFontFile extends Phaser.Loader.File
 	{
 		super(loader, {
 			type: 'webfont',
-			key: fontNames.toString()
-		})
+			key: fontNames.toString(),
+		});
 
-		this.fontNames = Array.isArray(fontNames) ? fontNames : [fontNames]
-		this.service = service
+		this.fontNames = Array.isArray(fontNames) ? fontNames : [fontNames];
+		this.service = service;
 	}
 
 	load()
 	{
 		const config = {
-			active: () => {
-				this.loader.nextFile(this, true)
-			}
-		}
+			active: () => this.loader.nextFile(this, true),
+		};
 
 		switch (this.service)
 		{
 			case 'google':
 				config['google'] = {
-					families: this.fontNames
-				}
+					families: this.fontNames,
+				};
 				break
 
 			default:
-				throw new Error('Unsupported font service')
+				throw new Error('Unsupported font service');
 		}
 
-		WebFont.load(config)
+		WebFont.load(config);
 	}
 }
